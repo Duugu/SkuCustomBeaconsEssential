3dd0cdd55307b7424a58322f723e71bc25ea3dac
Duugu <65570458+Duugu@users.noreply.github.com>
2022-09-01 00:08:03 +0200

    Initial commit

---
+++ Core.lua
@@ -0,0 +1,10 @@
+﻿local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:SetScript("OnEvent", function()
+   local SkuBeacon = LibStub("SkuBeacon-1.0")
+   if SkuBeacon then
+       SkuBeacon:RegisterSoundSet("Beacon 5", "Interface\\AddOns\\SkuCustomBeaconsEssential\\assets\\beacon5", 5, 30, "beacon5")
+       SkuBeacon:RegisterSoundSet("Beacon 6", "Interface\\AddOns\\SkuCustomBeaconsEssential\\assets\\beacon6", 5, 30, "beacon6")
+   end
+   f:UnregisterAllEvents()
+end)
